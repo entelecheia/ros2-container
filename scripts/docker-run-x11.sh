4d117f6bd65cfd1eff008445d19a51f8cf6125ed
Young Joon Lee <entelecheia@hotmail.com>
2024-04-06 14:53:39 +0900

    fix(docker-run-x11): fix DISPLAY environment variable assignment

--- scripts/docker-run-x11.sh
+++ scripts/docker-run-x11.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+DISPLAY=$(ip addr show docker0 | grep -Po 'inet \K[\d.]+'):0
+
 docker run -it --rm \
-    -e DISPLAY=host.docker.internal:0 \
+    -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     ghcr.io/entelecheia/ros2:latest-foxy-dsr \
     bash
