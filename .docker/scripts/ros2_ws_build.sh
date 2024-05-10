dd7743f2d8c37c1fd55ac4bb24c65e557a3b27e1
Young Joon Lee <entelecheia@hotmail.com>
2024-05-10 21:11:46 +0900

    chore: Update Dockerfile to include necessary ROS2 packages
    This commit updates the Dockerfile to include the necessary ROS2 packages: doosan-robot2, ros2_control, ros2_controllers, gazebo_ros2_control, and moveit_msgs. It also adds the fake_joint package for compatibility with our ROS2 package. The `fake_joint_driver_node.cpp` file is copied from the `src/doosan-robot2/common2/resource` directory to `src/fake_joint/fake_joint_driver/src` directory.

--- .docker/scripts/ros2_ws_build.sh
+++ .docker/scripts/ros2_ws_build.sh
@@ -2,6 +2,6 @@
 # this script should be executed only once when the docker container is created
 # shellcheck disable=SC1091
 source /opt/ros/foxy/setup.bash &&
-    cd /opt/ros_ws &&
+    cd /home/$USERNAME/$APP_DIRNAME &&
     colcon build --symlink-install &&
     . install/setup.bash
