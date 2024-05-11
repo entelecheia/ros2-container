d6542d27607ac00d8d17ab1f3db19ce43b4ac344
Young Joon Lee <entelecheia@hotmail.com>
2024-05-11 15:09:18 +0900

    chore: Update Dockerfile to remove unnecessary workspace creation

--- .docker/scripts/build-moveit.sh
+++ .docker/scripts/build-moveit.sh
@@ -9,5 +9,7 @@ rosdep update --rosdistro foxy
 rosdep install -r --from-paths src --ignore-src --rosdistro foxy -y
 # Command to activate the moveit package before colcon build
 rm -rf src/doosan-robot2/moveit_config_*/COLCON_IGNORE
-colcon build --symlink-install &&
-    . install/setup.bash
+colcon build --symlink-install \
+    --allow-overriding controller_interface controller_manager hardware_interface \
+    --cmake-args '-DCMAKE_BUILD_TYPE=Release' \
+    && . install/setup.bash
