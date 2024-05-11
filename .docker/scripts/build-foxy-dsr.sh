93d1e1f6e07d034aa8b4e95eb2d83480b7702fdb
Young Joon Lee <entelecheia@hotmail.com>
2024-05-12 05:22:49 +0900

    fix: Install dependencies for the Doosan Robot ROS2 package in build-foxy-dsr.sh

--- .docker/scripts/build-foxy-dsr.sh
+++ .docker/scripts/build-foxy-dsr.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+# Install dependencies for the Doosan Robot ROS2 package.
+sudo apt-get install -y \
+    ros-foxy-gazebo-ros-pkgs \
+    ros-foxy-control-toolbox \
+    ros-foxy-octomap-msgs \
+    ros-foxy-object-recognition-msgs \
+    ros-foxy-control-msgs
 # shellcheck disable=SC1091
 source /opt/ros/foxy/setup.bash &&
-    cd /home/$USERNAME/$APP_DIRNAME &&
+    cd "${APP_WORKSPACE_DIR}" &&
     colcon build --symlink-install &&
     . install/setup.bash
