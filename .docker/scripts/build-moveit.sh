566271f33eacec725e8d1d3f35ab746e67867c0d
Young Joon Lee <entelecheia@hotmail.com>
2024-05-11 13:12:10 +0900

    chore: Update Dockerfile to include necessary ROS2 packages and reset submodules

--- .docker/scripts/build-moveit.sh
+++ .docker/scripts/build-moveit.sh
@@ -3,7 +3,7 @@
 if [ ! -f /etc/ros/rosdep/sources.list.d/20-default.list ]; then
     sudo rosdep init
 fi
-rosdep update
+rosdep update --rosdistro foxy
 rosdep install -r --from-paths src --ignore-src --rosdistro foxy -y
 # Command to activate the moveit package before colcon build
 rm -rf src/doosan-robot2/moveit_config_*/COLCON_IGNORE
