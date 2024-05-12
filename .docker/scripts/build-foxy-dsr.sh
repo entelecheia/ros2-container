1e85d299e4403f0ddd813231a18c559ebbe9df00
Young Joon Lee <entelecheia@hotmail.com>
2024-05-12 16:36:43 +0900

    chore: Update Dockerfile to use specific scripts for cloning and resetting submodules in the foxy-dsr repository

--- .docker/scripts/build-foxy-dsr.sh
+++ .docker/scripts/build-foxy-dsr.sh
@@ -5,9 +5,10 @@ sudo apt-get install -y \
     ros-foxy-control-toolbox \
     ros-foxy-octomap-msgs \
     ros-foxy-object-recognition-msgs \
-    ros-foxy-control-msgs
+    ros-foxy-control-msgs \
+    ros-foxy-eigen-stl-containers
 # shellcheck disable=SC1091
 source /opt/ros/foxy/setup.bash &&
     cd "${APP_WORKSPACE_DIR}" &&
-    colcon build --symlink-install &&
+    colcon build --symlink-install --allow-overriding controller_interface hardware_interface &&
     . install/setup.bash
--- .docker/scripts/build-foxy-moveit.sh
+++ .docker/scripts/build-foxy-moveit.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-# Install dependencies for the MoveIt2 package.
-# sudo apt-get install -y \
-#     ros-foxy-chomp-motion-planner \
-#     ros-foxy-stomp
-    # ros-foxy-moveit-planners-chomp \
-    # ros-foxy-moveit-chomp-optimizer-adapter \
-    # ros-foxy-moveit-servo \
-    # ros-foxy-moveit-runtime \
-    # ros-foxy-moveit-ros-planning \
-    # ros-foxy-moveit-common \
-    # ros-foxy-warehouse-ros-mongo
+
 # shellcheck disable=SC1091
-source "${APP_WORKSPACE_DIR}/install/setup.bash"
+# source "${APP_WORKSPACE_DIR}/install/setup.bash"
+source /opt/ros/foxy/setup.bash
 cd "${APP_WORKSPACE_DIR}"
 # check /etc/ros/rosdep/sources.list.d/20-default.list and if not present, run sudo rosdep init
 if [ ! -f /etc/ros/rosdep/sources.list.d/20-default.list ]; then
@@ -25,34 +16,8 @@ rosdep install -r --from-paths src --ignore-src --rosdistro foxy -y
 # Command to activate the moveit package before colcon build
 rm -rf src/doosan-robot2/moveit_config_*/COLCON_IGNORE
 colcon build --symlink-install \
+    --allow-overriding controller_interface controller_manager hardware_interface \
     --cmake-args '-DCMAKE_BUILD_TYPE=Release' &&
     . install/setup.bash
 
     # --allow-overriding controller_interface controller_manager hardware_interface warehouse_ros \
-
-# python3-argcomplete python-lxml \
-# libpoco-dev libomp-dev libmongoclient-dev mongodb \
-# ros-foxy-desktop ros-foxy-ros-base \
-# ros-dev-tools ros-foxy-realtime-tools \
-# ros-foxy-xacro \
-# ros-foxy-object-recognition-msgs \
-# ros-foxy-control-msgs
-# ros-foxy-octomap \
-# ros-foxy-gazebo-ros ros-foxy-gazebo-ros-pkgs \
-# ros-foxy-eigen-stl-containers \
-# ros-foxy-random-numbers \
-# ros-foxy-moveit-resources-panda-description \
-# ros-foxy-moveit-resources-panda-moveit-config \
-# ros-foxy-moveit-resources-fanuc-description \
-# ros-foxy-moveit-resources-fanuc-moveit-config \
-# ros-foxy-moveit-resources-pr2-description \
-# ros-foxy-ros-testing \
-# ros-foxy-pybind11-vendor \
-# ros-foxy-ompl ros-foxy-urdfdom-py \
-# ros-foxy-gripper-controllers \
-# ros-foxy-joint-state-publisher-gui \
-# ros-foxy-joint-state-broadcaster \
-
-# ros-dev-tools libpoco-dev ros-foxy-control-msgs ros-foxy-realtime-tools \
-# ros-foxy-xacro ros-foxy-joint-state-publisher-gui ros-foxy-object-recognition-msgs \
-# ros-foxy-octomap-msgs ros-foxy-gazebo-ros ros-foxy-gazebo-ros-pkgs ros-foxy-control-toolbox \
